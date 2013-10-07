f98de7fcbe9013b1005e3e102d2c9379e73960d2
Marco Balduini <balduini.marco@gmail.com>
2013-10-07 11:04:20 +0200

    - lincense header updated - repository changed in pom.xml

--- csparql-cep-esper/src/main/java/eu/larkc/csparql/cep/esper/EsperQuery.java
+++ csparql-cep-esper/src/main/java/eu/larkc/csparql/cep/esper/EsperQuery.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2013 DEIB - Politecnico di Milano
+ * Copyright 2013 Davide Barbieri, Emanuele Della Valle, Marco Balduini
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * Acknowledgements:
+ * 
+ * This work was partially supported by the European project LarKC (FP7-215535)
  ******************************************************************************/
 /*
  * @(#)EsperQuery.java   1.0   15/set/2009
